8c71f68f754e9b813e96b3e3cff84fa1b6b36c36
mohamed <iaazi.mohamed.1998@gmail.com>
2025-05-06 16:13:06 +0100

    create enitiy for class produit

---
+++ src/main/java/com/medo/pharmacy/model/Produit.java
@@ -0,0 +1,20 @@
+package com.medo.pharmacy.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id ;
+    private  String nom ;
+    private  String quantite ;
+    private  String description ;
+
+}
