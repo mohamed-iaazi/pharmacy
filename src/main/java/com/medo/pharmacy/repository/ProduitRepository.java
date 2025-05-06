6b282f8a06046b4466dde2c6b0ad5280fc45a5f8
mohamed <iaazi.mohamed.1998@gmail.com>
2025-05-06 16:13:37 +0100

    create the produit repository

---
+++ src/main/java/com/medo/pharmacy/repository/ProduitRepository.java
@@ -0,0 +1,11 @@
+package com.medo.pharmacy.repository;
+
+import com.medo.pharmacy.model.Produit;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProduitRepository extends CrudRepository<Produit, Long> {
+    Produit getProduitsById(long id);
+}
